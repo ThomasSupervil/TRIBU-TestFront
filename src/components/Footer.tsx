44c3725d9d6720505c6dc32598e7adfcb95bdef7
ThomasSupervil <thomas.supervil@gmail.com>
2023-12-13 16:55:32 +0100

    feat: add link to socials

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,11 +6,11 @@ const Footer = () => {
        <img src='/public/assets/logoBagel.png' alt='' />
 
        <div className='footer--socials'>
-        <img src='/public/assets/icons/instagram.png' alt='' />
-        <img src='/public/assets/icons/facebook.png' alt='' />
-        <img src='/public/assets/icons/twitter.png' alt='' />
-        <img src='/public/assets/icons/linkedin.png' alt='' />
-        <img src='/public/assets/icons/ubereats.png' alt='' />
+        <a href='/'><img src='/public/assets/icons/instagram.png' alt='' /></a>
+        <a href='/'><img src='/public/assets/icons/facebook.png' alt='' /></a>
+        <a href='/'><img src='/public/assets/icons/twitter.png' alt='' /></a>
+        <a href='/'><img src='/public/assets/icons/linkedin.png' alt='' /></a>
+        <a href='/'><img src='/public/assets/icons/ubereats.png' alt='' /></a>
        </div>
 
        <nav>
